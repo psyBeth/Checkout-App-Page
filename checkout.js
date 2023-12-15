fb2407d9f89199c68381975cca054a2eab61e78d
Betul <betulkoru06@gmail.com>
2023-12-15 19:09:27 +0300

    quantity 1

--- checkout.js
+++ checkout.js
@@ -3,9 +3,30 @@ const products = document.querySelector(".products")
 
 //? Delete Products button event
 deleteProducts.addEventListener("click", () => {
-    if(confirm("are you sure you want to delete products?")){
+    if(confirm("Are you sure you want to delete products?")){
         products.textContent = "No product"
         products.classList.add("no-product")
         document.querySelector(".delete-div").style.display = "none"
     }
 });
+
+products.addEventListener("click", (e) => {
+    console.log(e.target);
+
+    // if(e.target.classList.contains("fa-minus")){
+    //     alert("minus")
+    // } else if (e.target.classList.contains("fa-plus")){
+    //     alert("plus")
+    // } else if (e.target.classList.contains("fa-trash-can")){
+    //     alert("remove")
+    // } else {
+    //     alert("other")
+    // }
+
+    if(e.target.classList.contains("fa-plus")){
+       // document.getElementById("quantity").innerText++
+       e.target.addEventListener.previousElementSibling.innerText++
+    }
+
+
+})
