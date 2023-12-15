ad940bfda0be385f9b84f12fab71e5cdab1f75d1
Betul <betulkoru06@gmail.com>
2023-12-15 17:28:21 +0300

    delete product

--- checkout.js
+++ checkout.js
@@ -1,8 +1,11 @@
 const deleteProducts = document.querySelector(".delete-div .fa-trash-can")
+const products = document.querySelector(".products")
 
 //? Delete Products button event
 deleteProducts.addEventListener("click", () => {
-
-
-    
-})
+    if(confirm("are you sure you want to delete products?")){
+        products.textContent = "No product"
+        products.classList.add("no-product")
+        document.querySelector(".delete-div").style.display = "none"
+    }
+});
